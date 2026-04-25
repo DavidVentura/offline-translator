b8163e77400072e0f6417ee311808d64477178fc
David Ventura <david@davidv.dev>
2026-04-25 20:55:26 +0200

    add release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
     applicationId = "dev.davidv.translator"
     minSdk = 21
     targetSdk = 34
-    versionCode = 13
-    versionName = "0.3.2"
+    versionCode = 14
+    versionName = "0.3.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
